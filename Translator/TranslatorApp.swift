e81ef99d9532100a373059986d56a0bc285f19c5
Ihor <futureseniordev@outlook.com>
2025-02-10 13:38:35 +0200

    Initial Commit

---
+++ Translator/TranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslatorApp.swift
+//  Translator
+//
+//  Created by Igor Plastun on 10.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct TranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
